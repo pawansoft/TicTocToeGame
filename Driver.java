b97cf8763fd46b2ba7339ad906d98c151427add1
Pawan Kumar <pk.soft29@gmail.com>
2020-10-08 11:30:53 +0530

    Added: print the board functionality

--- Driver.java
+++ Driver.java
@@ -2,11 +2,25 @@ import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
-
+            Scanner sc=new Scanner(System.in);
+            int option=sc.nextInt();
          TickTocToeGame tickTocToeGame=new TickTocToeGame();
-        char []board=tickTocToeGame.takingInput();
-
-       tickTocToeGame.userInput();
+        System.out.println("1: for initialize the board, 2: tahke input , 3 : print the board");
+        switch (option)
+        {
+            case 1:
+                char []board=tickTocToeGame.takingInput();
+                break;
+            case 2:
+                tickTocToeGame.userInput();
+                break;
+            case 3:
+                tickTocToeGame.printBoard();
+                break;
+            default:
+                System.out.println("please provide valid option");
+                break;
+        }
     }
 }
 
--- TickTocToeGame.java
+++ TickTocToeGame.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class TickTocToeGame {
+    char board [] = new char[10];
     public char[] takingInput()
     {
-        char board [] = new char[10];
+
         for(int i=1;i< board.length;i++)
         {
             board[i]=' ';
@@ -13,8 +14,8 @@ public class TickTocToeGame {
     public char userInput()
     {
         Scanner sc=new Scanner(System.in);
-        char userValue=sc.next().charAt(0);
-        if(userValue == '0' || userValue =='X')
+        char userValue=sc.next().toUpperCase().charAt(0);
+        if(userValue == '0' || userValue == 'X')
         {
             return userValue;
         }
@@ -23,8 +24,15 @@ public class TickTocToeGame {
             System.out.println("Please provide valid input");
         }
         return 0;
-
-
+    }
+    public void printBoard() {
+        System.out.println("-----------------------");
+        System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
+        System.out.println("|--|--|--|");
+        System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+        System.out.println("|--|--|--|");
+        System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+        System.out.println("-----------------------");
     }
 }
 
