ef0ad3e5141b62240cf0e227b6d6825be76977cc
Pawan Kumar <pk.soft29@gmail.com>
2020-10-08 13:21:00 +0530

    Added : toss for first input between computer and user funcnality added

--- Driver.java
+++ Driver.java
@@ -1,23 +1,45 @@
 import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        TickTocToeGame tickTocToeGame=new TickTocToeGame();
-//        char []board=tickTocToeGame.takingInput();
-          char userOption=tickTocToeGame.userInput();
-
+        Scanner sc = new Scanner(System.in);
+        TickTocToeGame tickTocToeGame = new TickTocToeGame();
+        char computerOption;
+        char []board = tickTocToeGame.takingInput();
+          char userOption = tickTocToeGame.userInput();
+          if(userOption == 'X')
+          {
+              computerOption = '0';
+          }
+          else
+          {
+              computerOption = 'X';
+          }
         //Checking that index is free or not
-        System.out.println("provide the index number");
-        int index=sc.nextInt();
-        boolean check =tickTocToeGame.checkIndex(index);
-        if(check == true) {
-            tickTocToeGame.makeAMove(index, userOption);
+        boolean terminate = false;
+        while(terminate != true)
+        {
+            System.out.println("provide the index number");
+            int index = sc.nextInt();
+            boolean check = tickTocToeGame.checkIndex(index);
+            if(check == true) {
+                tickTocToeGame.makeAMove(index, userOption);
+                terminate = true;
+            }
+            else
+            {
+                System.out.println("index is not available");
+            }
+            tickTocToeGame.printBoard();
+        }
+        int winner = tickTocToeGame.toss();
+        if(winner == 1)
+        {
+            System.out.println("Computers will provide first input");
         }
         else
         {
-                System.out.println("index is not available");
+            System.out.println("User will provide first input");
         }
-        tickTocToeGame.printBoard();
 
     }
 }
--- TickTocToeGame.java
+++ TickTocToeGame.java
@@ -1,13 +1,13 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TickTocToeGame {
     char board [] = new char[10];
     public char[] takingInput()
     {
-
-        for(int i=1;i< board.length;i++)
+        for(int i = 1;i < board.length;i++)
         {
-            board[i]= 0;
+            board[i] = 0;
         }
         return board;
     }
@@ -29,9 +29,9 @@ public class TickTocToeGame {
     public void printBoard() {
         System.out.println("-----------------------");
         System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
-        System.out.println("|----|----|----|");
+        System.out.println("|---|---|---|");
         System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
-        System.out.println("|----|----|----|");
+        System.out.println("|---|---|---|");
         System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
         System.out.println("-----------------------");
     }
@@ -40,19 +40,24 @@ public class TickTocToeGame {
         boolean check;
         if (board[index] == 0)
         {
-            check =true;
+            check = true;
         }
         else
         {
-            check=false;
+            check = false;
         }
         return check;
     }
     public void makeAMove(int index, char useInput)
     {
-        board[index]=useInput;
+        board[index] = useInput;
     }
-
+    public int toss()
+    {
+        Random rn=new Random();
+        int win = rn.nextInt(2);
+        return win;
+    }
 }
 
 
