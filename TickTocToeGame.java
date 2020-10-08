363781d7a60194996575db78acbb944ab41000c4
Pawan Kumar <pk.soft29@gmail.com>
2020-10-08 10:03:30 +0530

    UC1_tictocToe

---
+++ TickTocToeGame.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TickTocToeGame {
+    Scanner sc=new Scanner(System.in);
+    static char[] board;
+    String turn;
+    public void takingInput()
+    {
+
+    }
+}
+
