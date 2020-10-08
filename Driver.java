1e11d727d3f3130ad6e0acd98cf71a0a39074982
Pawan Kumar <pk.soft29@gmail.com>
2020-10-08 10:41:50 +0530

    Added: Taking input from user Funcnality

--- Driver.java
+++ Driver.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
 
+         TickTocToeGame tickTocToeGame=new TickTocToeGame();
+        char []board=tickTocToeGame.takingInput();
+
+       tickTocToeGame.userInput();
     }
 }
+
--- TickTocToeGame.java
+++ TickTocToeGame.java
@@ -1,12 +1,31 @@
 import java.util.Scanner;
 
 public class TickTocToeGame {
-    Scanner sc=new Scanner(System.in);
-    static char[] board;
-    String turn;
-    public void takingInput()
+    public char[] takingInput()
     {
+        char board [] = new char[10];
+        for(int i=1;i< board.length;i++)
+        {
+            board[i]=' ';
+        }
+        return board;
+    }
+    public char userInput()
+    {
+        Scanner sc=new Scanner(System.in);
+        char userValue=sc.next().charAt(0);
+        if(userValue == '0' || userValue =='X')
+        {
+            return userValue;
+        }
+        else
+        {
+            System.out.println("Please provide valid input");
+        }
+        return 0;
+
 
     }
 }
+
 
